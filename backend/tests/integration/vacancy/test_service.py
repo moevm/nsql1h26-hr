32f9048dc2940b0c3216f39d0a00da7393b4e1af
VBStudi0s <vladybout@gmail.com>
2026-04-17 00:39:17 +0300

    feat: patch vacancy additional test

--- backend/tests/integration/vacancy/test_service.py
+++ backend/tests/integration/vacancy/test_service.py
@@ -47,3 +47,20 @@ async def test_patch_vacancy_ok(vacancy_service):
     assert patched_vacancy.status == VacancyStatus.CLOSED
     assert patched_vacancy.closed_at is not None
 
+
+async def test_patch_vacancy_ok2(vacancy_service):
+    test_vacancy = VacancyCreate(
+        title="Test vacancy",
+        description="Test Vacancy Description"
+    )
+    vacancy = await vacancy_service.create_vacancy(test_vacancy)
+    vacancy_patch = VacancyPatch()
+    vacancy_patch.title = "Title 2"
+    vacancy_patch.description = "D 2"
+    patched_vacancy = await vacancy_service.patch_vacancy(vacancy.id,
+                                                          vacancy_patch)
+    assert patched_vacancy is not None
+    assert patched_vacancy.title == vacancy_patch.title
+    assert patched_vacancy.description == vacancy_patch.description
+    assert patched_vacancy.status == VacancyStatus.OPEN
+    assert patched_vacancy.closed_at is None
