0ed8103407ecce15cd8113fc9d6515a4ec039ed9
VBStudi0s <vladybout@gmail.com>
2026-05-10 16:27:35 +0300

    fix: candidate patch url

--- backend/app/models/system_backup.py
+++ backend/app/models/system_backup.py
@@ -1,11 +1,11 @@
 from typing import List
 from pydantic import BaseModel
-from user import UserResponse
-from vacancy import VacancyResponse
-from test_task import TestTaskResponse
-from candidate import CandidateResponse
-from interview import InterviewResponse
-from offer import OfferResponse
+from app.models.user import UserResponse
+from app.models.vacancy import VacancyResponse
+from app.models.test_task import TestTaskResponse
+from app.models.candidate import CandidateResponse
+from app.models.interview import InterviewResponse
+from app.models.offer import OfferResponse
 
 
 class SystemBackup(BaseModel):
